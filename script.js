ffa7b1459d8e70211fdfff822824df8caa2c42cb
Munawar Johar <106137102+MunawarJohar@users.noreply.github.com>
2022-11-14 12:07:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var white = document.getElementById("white_c");
+
+white.onclick = function() {
+    document.body.classList.toggle("white_theme")
+
+}
+var dark = document.getElementById("dark_theme");
+dark.onclick = function() {
+    document.body.classList.toggle("dark_theme")
+
+}
